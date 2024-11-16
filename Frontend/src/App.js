278a14d7c31acf487d30f51931a4bc18a22d1d8c
latebyciclenz <latebyciclenz@gmail.com>
2024-11-16 16:49:22 +0000

    feat(Frontend)
    Fixed Styling

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
 import Sidebar from "./components/Sidebar.js";
 import JobList from "./components/JobList.js";
-import "./styles/App.css";
+import './components/styles/App.css';
 
 function App() {
   return (
-    <div className="app">
+    <div className="app"> 
+      <header className="fixed-header"> {/* Add a class for styling */}
+        <h1>OkComputer</h1>
+      </header>
       <Sidebar />
       <JobList />
     </div>
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/App.css"; // Ensure the correct path for App.css
+import './components/styles/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
